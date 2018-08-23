fad6d5f65ef0786a038755699869396b422cba6e
Sheckerol <checol70@gmail.com>
2018-08-22 18:44:58 -0700

    fixed small bugs

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,17 +11,12 @@ module.exports = function(app){
         db.Note.create(req.body)
         .then(function(dbNote) {
             console.log(dbNote)
-          // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-          // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-          // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
           return db.Article.findOneAndUpdate({ _id: req.params.artId }, {$push: { note: dbNote._id} }, { new: true });
         })
         .then(function(dbArticle) {
-          // If we were able to successfully update an Article, send it back to the client
           res.json(dbArticle);
         })
         .catch(function(err) {
-          // If an error occurred, send it to the client
           res.json(err);
         });
     })
@@ -60,7 +55,9 @@ module.exports = function(app){
                 result.link = $(this)
                     .children("a")
                     .attr("href");
-    
+                if(result.link.startsWith("/")){
+                    result.link = `http://www.echojs.com/${result.link}`
+                }
                 // Create a new Article using the `result` object built from scraping
                 db.Article.create(result)
                     .then(function (dbArticle) {
